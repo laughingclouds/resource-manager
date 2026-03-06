af456e4f8f6976bd52efe3de14a5919f3da67cd8
Hemant Bhandari <zenithtaiga@gmail.com>
2026-03-06 18:23:38 +0530

    __dirname and __filename

--- src/electron/main.mjs
+++ src/electron/main.mjs
@@ -2,6 +2,11 @@ import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { isDev } from './util.mjs';
 import { pollResources } from './ResourceManager.mjs';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const createWindow = () => {
