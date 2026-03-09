c5b022da9c914d2496630bd5dc6dd65aa95b6153
Hemant Bhandari <zenithtaiga@gmail.com>
2026-03-09 13:22:21 +0530

    Validate the event
    We need to validate in the backend only

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,7 +1,9 @@
 // isDev returns true if development environment, false otherwise
 // import { loadEnvFile } from 'process';
 
-import { ipcMain, WebContents } from "electron";
+import { ipcMain, WebContents, WebFrameMain } from "electron";
+import { getUIPath } from "./pathResolver.js";
+import { pathToFileURL } from "url";
 
 export function isDev() {
   // TODO: make it so the env is loaded on its own
@@ -15,7 +17,13 @@ export function ipcHandle<Key extends keyof EventPayloadMapping>(
   key: Key,
   handler: () => EventPayloadMapping[Key],
 ) {
-  ipcMain.handle(key, () => handler());
+  ipcMain.handle(key, (event) => {
+    if (!event.senderFrame) return;
+
+    validateEventFrame(event.senderFrame!);
+
+    return handler();
+  });
 }
 
 export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
@@ -24,4 +32,19 @@ export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
   payload: EventPayloadMapping[Key],
 ) {
   webContents.send(key, payload);
+}
+
+/**
+ * Throw error if frame is not loaded from the UI Path it's supposed to load from.
+ */
+export function validateEventFrame(frame: WebFrameMain) {
+  console.log(frame.url);
+
+  if (isDev() && new URL(frame.url).host === "localhost:5123") {
+    return;
+  }
+
+  if (frame.url !== pathToFileURL(getUIPath()).toString()) {
+    throw new Error("Malicious Event");
+  }
 }
