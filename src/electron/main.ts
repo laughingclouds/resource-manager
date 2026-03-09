fa60cd566448a56d7046e779ed035a66e7baeeac
Hemant Bhandari <zenithtaiga@gmail.com>
2026-03-09 13:22:39 +0530

    Prettier format

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -9,35 +9,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
-	const mainWindow = new BrowserWindow({
-		icon: path.join(app.getAppPath(), "desktopIcon.png"),
-		webPreferences: {
-			preload: getPreloadPath(),
-		},
-	});
-	if (isDev()) {
-		mainWindow.loadURL("http://localhost:5123");
-	} else {
-		mainWindow.loadFile(getUIPath());
-	}
+  const mainWindow = new BrowserWindow({
+    icon: path.join(app.getAppPath(), "desktopIcon.png"),
+    webPreferences: {
+      preload: getPreloadPath(),
+    },
+  });
+  if (isDev()) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    mainWindow.loadFile(getUIPath());
+  }
 
-	return mainWindow;
+  return mainWindow;
 };
 
 app.whenReady().then(() => {
-	const mainWindow = createWindow();
+  const mainWindow = createWindow();
 
-	pollResources(mainWindow);
+  pollResources(mainWindow);
 
-	ipcHandle('getStaticData', () => {
-		return getStaticData();
-	});
+  ipcHandle("getStaticData", () => {
+    return getStaticData();
+  });
 
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
